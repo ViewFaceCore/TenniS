4d4a06296db800e9bbb7204ba166934e883db2ee
withsalt <sysrun@163.com>
2022-11-22 23:38:46 +0800

    fix: 错误修改

--- src/compiler/argparse.cpp
+++ src/compiler/argparse.cpp
@@ -10,62 +10,80 @@
 
 #include <algorithm>
 
-namespace ts {
-    static std::string stand1(const std::string &str) {
+namespace ts
+{
+    static std::string stand1(const std::string &str)
+    {
         static std::regex regex(R"(^\s*(\S*)\s*$)");
         std::smatch match;
         std::string cvt;
-        if(std::regex_match(str, match, regex)) {
+        if (std::regex_match(str, match, regex))
+        {
             cvt = match[1];
-        } else {
+        }
+        else
+        {
             cvt = str;
         }
-        std::transform(cvt.begin(), cvt.end(), cvt.begin(), std::tolower);
+        std::transform(cvt.begin(), cvt.end(), cvt.begin(), ::tolower);
         return cvt;
     }
 
-    static std::string stand2(const std::string &str) {
+    static std::string stand2(const std::string &str)
+    {
         static std::regex regex(R"(^[_-]*([^_-]*)[_-]*$)");
         std::smatch match;
-        if(std::regex_match(str, match, regex)) {
+        if (std::regex_match(str, match, regex))
+        {
             return match[1];
-        } else {
+        }
+        else
+        {
             return str;
         }
     }
 
-    ArgParser::ArgParser() {
+    ArgParser::ArgParser()
+    {
         m_map_device.insert(std::make_pair("", "cpu"));
         m_map_device.insert(std::make_pair("cuda", "gpu"));
     }
 
     void ts::ArgParser::add(const std::vector<std::string> &arg, const std::vector<std::string> &neg_arg,
-                            bool default_value) {
-        if (arg.empty()) {
+                            bool default_value)
+    {
+        if (arg.empty())
+        {
             TS_LOG_ERROR << "param@1 can not be empty." << eject;
         }
 
         const std::string &true_arg_name = arg.front();
         m_true_arg_names.insert(std::make_pair(true_arg_name, true_arg_name));
-        for (auto it = arg.begin() + 1; it != arg.end(); ++it) {
+        for (auto it = arg.begin() + 1; it != arg.end(); ++it)
+        {
             m_true_arg_names.insert(std::make_pair(*it, true_arg_name));
         }
-        for (auto it = neg_arg.begin(); it != neg_arg.end(); ++it) {
+        for (auto it = neg_arg.begin(); it != neg_arg.end(); ++it)
+        {
             m_false_arg_names.insert(std::make_pair(*it, true_arg_name));
         }
-        if (default_value) {
+        if (default_value)
+        {
             m_arg_value.insert(std::make_pair(true_arg_name, default_value));
         }
     }
 
-    bool ArgParser::set(const std::string &arg) {
+    bool ArgParser::set(const std::string &arg)
+    {
         auto true_it = m_true_arg_names.find(arg);
-        if (true_it != m_true_arg_names.end()) {
+        if (true_it != m_true_arg_names.end())
+        {
             m_arg_value[true_it->second] = true;
             return true;
         }
         auto false_it = m_false_arg_names.find(arg);
-        if (false_it != m_false_arg_names.end()) {
+        if (false_it != m_false_arg_names.end())
+        {
             // m_arg_value.erase(false_it->second);
             m_arg_value[false_it->second] = false;
             return true;
@@ -73,23 +91,29 @@ namespace ts {
         return false;
     }
 
-    bool ArgParser::get(const std::string &arg) const {
+    bool ArgParser::get(const std::string &arg) const
+    {
         auto it = m_arg_value.find(arg);
-        if (it != m_arg_value.end()) {
+        if (it != m_arg_value.end())
+        {
             return it->second;
         }
         return false;
     }
 
-    void ArgParser::parse(const std::string &args) {
+    void ArgParser::parse(const std::string &args)
+    {
         static std::regex regex_map(R"(^-[vV]:([^:]*):([^:]*)$)");
 
         auto params = Split(args, " \t\r\n");
         std::smatch match_v;
-        for (auto &param : params) {
-            if (param.empty()) continue;
+        for (auto &param : params)
+        {
+            if (param.empty())
+                continue;
             // check if it's device map
-            if(std::regex_match(param, match_v, regex_map)) {
+            if (std::regex_match(param, match_v, regex_map))
+            {
                 this->m_map_device[stand1(match_v[1])] = stand1(match_v[2]);
                 continue;
             }
@@ -98,13 +122,18 @@ namespace ts {
         }
     }
 
-    std::string ArgParser::map_device(const std::string &device) {
+    std::string ArgParser::map_device(const std::string &device)
+    {
         auto stand_device = stand1(device);
-        while (true) {
+        while (true)
+        {
             auto it = this->m_map_device.find(stand_device);
-            if (it != this->m_map_device.end()) {
+            if (it != this->m_map_device.end())
+            {
                 stand_device = it->second;
-            } else {
+            }
+            else
+            {
                 return stand2(stand_device);
             }
         }
